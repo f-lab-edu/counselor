029f01eaed90f7bd9a49878245abfd9cb44ca2e1
hyejin <kiela929@naver.com>
2025-09-29 00:11:14 +0900

    module 제거

--- core/src/main/java/com/hyejin/counselor/core/service/UserService.java
+++ core/src/main/java/com/hyejin/counselor/core/service/UserService.java
@@ -1,17 +1,18 @@
 package com.hyejin.counselor.core.service;
 
-import com.hyejin.counselor.core.module.User;
+
+import com.hyejin.counselor.core.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
-    private final User userModule;
+    private final UserRepository repository;
 
     public boolean save(com.hyejin.counselor.core.entity.User user) {
         boolean result = false;
-        if(userModule.save(user)!=null) result = true;
+        if(repository.save(user)!=null) result = true;
         return result;
     }
 }
